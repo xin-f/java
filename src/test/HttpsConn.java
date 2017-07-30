bb8ac80710f5d124a52a9a82280a2fbf2ded9fdf
xin-f <sxfv0920@163.com>
2017-07-30 14:30:51 +0800

    print some detailed info of certificate  Polymorphism

--- src/test/HttpsConn.java
+++ src/test/HttpsConn.java
@@ -2,8 +2,10 @@ package test;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyManagementException;
@@ -41,24 +43,39 @@ public class HttpsConn {
 //			SSLSession sc = scc.getSession(new byte[]);
 //			Enumeration<byte[]>	   getIds()
 			Enumeration<byte[]> id_c = scc.getIds();
-			Enumeration<byte[]> id_s = scs.getIds();
+//			Enumeration<byte[]> id_s = scs.getIds();
 			byte[] b1 = null;	
 			byte[] b2 = null;
 //			while(id_c.hasMoreElements()) {
 				b1 = id_c.nextElement();
 //				b2 = id_s.nextElement();
-//				for (byte x:b) {
+//				for (byte x:b) { 
 //					System.out.print(Byte.toString(x)+",");
 //				}
 //			}
-			SSLSession ss = scc.getSession(b1);
-			System.out.println(ss.getCipherSuite());
-			System.out.println(ss.getLocalCertificates());
-			System.out.println(ss.getPeerCertificates());
-			System.out.println(ss.getPeerCertificateChain());
-			System.out.println(ss.getPeerCertificates()[0].toString());
-			System.out.println("===================================");
-			System.out.println(ss.getPeerCertificateChain()[0].toString());
+			FileOutputStream fos = new FileOutputStream("aaa.txt");
+			PrintWriter pw = new PrintWriter(fos);
+			SSLSession sslSession = scc.getSession(b1);
+			X509Certificate cert = (X509Certificate) sslSession.getPeerCertificates()[0];
+			System.out.println(sslSession.getCipherSuite());
+			System.out.println(sslSession.getLocalCertificates()+"  l");//读不到东西.
+			System.out.println(sslSession.getPeerCertificates()+"   p");
+			System.out.println(sslSession.getPeerCertificateChain());
+//			System.out.println(ss.getPeerCertificates()[0].toString());
+//			System.out.println("===================================");
+//			System.out.println(ss.getPeerCertificateChain()[0].toString());
+//			pw.write(ss.getPeerCertificates()[0].toString());
+//			pw.close();
+//			fos.close();
+			System.out.println("读取Cer证书信息...");  
+	        System.out.println("x509Certificate_SerialNumber_序列号___:"+cert.getSerialNumber());  
+	        System.out.println("x509Certificate_getIssuerDN_发布方标识名___:"+cert.getIssuerDN());   
+	        System.out.println("x509Certificate_getSubjectDN_主体标识___:"+cert.getSubjectDN());  
+	        System.out.println("x509Certificate_getSigAlgOID_证书算法OID字符串___:"+cert.getSigAlgOID());  
+	        System.out.println("x509Certificate_getNotBefore_证书有效期___:"+cert.getNotAfter());  
+	        System.out.println("x509Certificate_getSigAlgName_签名算法___:"+cert.getSigAlgName());  
+	        System.out.println("x509Certificate_getVersion_版本号___:"+cert.getVersion());  
+	        System.out.println("x509Certificate_getPublicKey_公钥___:"+cert.getPublicKey());  
 
 			
 		} catch (MalformedURLException e) {
