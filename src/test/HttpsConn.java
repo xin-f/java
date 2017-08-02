ae7e958bc29d23d8ae95e832553fefa48cfda667
xin-f <sxfv0920@163.com>
2017-08-02 09:58:58 +0100

    generate 1 to 24 length string

--- src/test/HttpsConn.java
+++ src/test/HttpsConn.java
@@ -18,6 +18,8 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Enumeration;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -28,12 +30,31 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 public class HttpsConn {
-	public static void main(String[] arg) throws IOException{
-		URL url = new URL("https://www.baidu.com");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setConnectTimeout(100);
-		conn.connect();
-//		System.out.println(host.isReachable(10));		
+	public static void main(String[] arg) {
+		Timer t = new Timer();
+		TimerTask timerTask = new TimerTask(){
+			@Override
+			public void run() {
+				getString();
+			}			
+		};
+		t.schedule(timerTask, 0, 100);
 	}
+	public static void getString() {
+		String cha = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+		String upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String low = "abcdefghijklmnopqrstuvwxyz";
+		String dig = "0123456789";
+		String all = cha + upp + low + dig;
+		int length = all.length();		
+		
+		StringBuffer target = new StringBuffer();
+		int random = (int) Math.round(Math.random()*23); //目标字符串,1到24位.
+		for(int i = 0; i <= random; i++){
+			int random_all = (int) Math.round(Math.random()*(length-1)); //字符串的index
+			target.append(all.charAt(random_all));
+		}		
+		System.out.println(target);}
+	
 }
 
